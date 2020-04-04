bad72892b18e9e2620d63b5e4b2817ba256851cf
Patrice <info@patrice.codes>
2020-04-04 23:01:36 +0200

    return the newest items by default

--- src/services/Service.js
+++ src/services/Service.js
@@ -33,6 +33,7 @@ class Service {
         try {
             let items = await this.model
                 .find(query)
+                .sort({createdAt: -1})
                 .skip(skip)
                 .limit(limit);
             let total = await this.model.count();
